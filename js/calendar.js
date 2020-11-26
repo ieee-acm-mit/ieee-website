da450bb14aed36dcf5c998a98fc4f2bc1b7fffab
Ophelia <wattinytoffee@gmail.com>
2020-11-25 19:23:54 -0800

    reformat export

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,5 @@
-import API_KEY from '../secrets';
+import secret from '../secrets';
+const API_KEY = secret.API_KEY;
 const MONTHS = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August",
 9: "September", 10: "October", 11:"November", 12: "December"};
 
