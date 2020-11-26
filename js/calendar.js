8fe1cd5515b79998f10c0eae507401446c20cc2a
Ophelia <wattinytoffee@gmail.com>
2020-11-25 19:20:42 -0800

    move

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,4 @@
-const API_KEY = 'AIzaSyDpllO5R79dXtxGpDPplkoxsG3S64X9Z2M';
+import API_KEY from '../secrets';
 const MONTHS = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August",
 9: "September", 10: "October", 11:"November", 12: "December"};
 
