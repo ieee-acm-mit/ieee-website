8f7fe1dc318286cbcc4205efdc376e943e764e76
Ophelia <wattinytoffee@gmail.com>
2020-11-25 19:27:39 -0800

    fix transition

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,4 @@
-import secret from '../secrets';
+import secret from '../secrets.js';
 const API_KEY = secret.API_KEY;
 const MONTHS = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August",
 9: "September", 10: "October", 11:"November", 12: "December"};
