39addc07b98392f1cbffece7acb471dda9176733
Ophelia <wattinytoffee@gmail.com>
2021-04-17 17:56:10 -0400

    test css

--- page/index.php
+++ page/index.php
@@ -1,25 +1,20 @@
 <html>
-<head><title>Diagnostics</title></head>
+<head>
+<title>MIT IEEE Event Tracker</title>
+<link rel="stylesheet" href="../css/tracker.css">
+</head>
 <body>
 <h1>Browser Detection</h1>
 <p>
-<?php
- print 'You are using <b>' . $_SERVER['HTTP_USER_AGENT'] . '</b> from <b>'
-     . $_SERVER['REMOTE_ADDR'] . '</b> (<b>'
-     . gethostbyaddr($_SERVER['REMOTE_ADDR']) . '</b>).';
-?>
 </p>
-<h1>Certificate Detection</h1>
+<h1>Event Tracker</h1>
 <p>
 <?php
  if (@$_SERVER['SSL_CLIENT_S_DN_CN']) {
  $parts = explode('@', $_SERVER['SSL_CLIENT_S_DN_Email']);
  $user = $parts[0];
-  echo 'Welcome, <b>' . $_SERVER['SSL_CLIENT_S_DN_CN'] . '</b>.<br>'
-      . 'A certificate for <b>' . $user . '</b>'
-      . ', issued by the <b>' . $_SERVER['SSL_CLIENT_I_DN_O']
-      . '</b>, is correctly installed on your browser.<br>'
-      . 'Your certificate will expire on ' . $_SERVER['SSL_CLIENT_V_END'] . '.';
+  echo 'Hello <b>' . $_SERVER['SSL_CLIENT_S_DN_CN'] . '</b>!<br>'
+      . 'A certificate for <b>' . $user . '</b>';
  } else {
   ?>No certificate has been detected. Please ensure you are accessing
   <a href="https://geofft.scripts.mit.edu:444/detect.php">http<b>s</b>://geofft.scripts.mit.edu<b>:444</b>/detect.php</a>.<?php } ?>
@@ -27,20 +22,13 @@
 <h1>Server Detection</h1>
 <p>
 <?php
- exec('/sbin/ifconfig eth0', $ifconfig);
- $server_ip = preg_replace('/.*inet addr:([0-9\.]*).*/', '$1', $ifconfig[1]);
- print 'You are currently connected to <b>'
-     . gethostbyaddr($server_ip) . '</b> (' . $server_ip
-     . ').<br><pre>';
- system('/sbin/ifconfig eth0');
- print '</pre>';
-?>
-<?php
  $csvFile = file('https://docs.google.com/spreadsheets/d/e/2PACX-1vSMkgV9ZcEuokH-P_CLKHNI8rSDO0PDfRZ3O9X9fqfIW7-HlUPcHhv0sG0iG3Fwvr3M5AqoAXPLwAaw/pub?output=csv');
  $count = 0;
+
 foreach ($csvFile as $line) {
 	$data = str_getcsv($line);
 	if ($data[0] == $user){
+	    echo '<li>' . $data[0] . '</li>';
 		$count++;
 	}
 }
