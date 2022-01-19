5c0e7132dd7621e6e0b7e7c773ca5abedef0a656
Julia Xia <juliaxia@mit.edu>
2022-01-18 22:08:18 -0500

    original modification to calendar debugging #4

--- js/calendar.js
+++ js/calendar.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
     $.get(URL, function(data, status){
         const events = data.items;
         console.log(events)
-        events.sort((a,b)=>{return a.start.dateTime - b.start.dateTime})
+        events.sort((a,b)=>{return new Date(a.start.dateTime) - new Date(b.start.dateTime)})
         console.log(events)
         let calendar_block = document.getElementById('calendarContainer');
         if (events.length != 0){
