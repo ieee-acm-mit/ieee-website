a61dee9bb2bd64245bf32ef0ea47c6e6a7b4697c
Jophy <jophyyjh@gmail.com>
2024-02-17 17:08:07 -0500

    Implemented event showcase, integrated on homepage

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,48 +1,27 @@
-import 'react-slideshow-image/dist/styles.css'
 import React from 'react';
-import { Slide } from 'react-slideshow-image';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import './ImageSlider.css';
 
-const spanStyle = {
-  padding: '20px',
-  background: '#efefef',
-  color: '#000000'
-}
 
-const divStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundSize: 'cover',
-  height: '400px'
-}
-const slideImages = [
-  {
-    url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-    caption: 'Slide 1'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
-    caption: 'Slide 2'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-    caption: 'Slide 3'
-  },
-];
-
-const Slideshow = () => {
+const Slideshow = (props) => {
+    let slideImages = props.slideImages;
     return (
-      <div className="slide-container">
-        <Slide>
-         {slideImages.map((slideImage, index)=> (
-            <div key={index}>
-              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
-                <span style={spanStyle}>{slideImage.caption}</span>
+      <Fade autoPlay={true} duration={6000} indicators={true}>
+        {slideImages.map((slideImage, index)=> (
+          <div key={index}>
+            <div className="slider-content"
+                  style={{ backgroundImage: `url(${slideImage.url})` }}>
+              <div className="overlay"></div>
+              <div className="container caption">
+                <h1>MIT IEEE/ACM</h1>
+                <p>{ slideImage.caption }</p>
               </div>
             </div>
-          ))} 
-        </Slide>
-      </div>
+          </div>
+        ))} 
+      </Fade>
     )
 }
+
 export default Slideshow;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,37 +1,70 @@
 import React from 'react';
-import Slideshow from '../components/ImageSlider';
 import "../css/HomePage.css";
+import {eventSlides, EventShowcase} from '../components/EventsShowcase';
+
+
+const introSlides = [
+  {
+    url: 'images/slideshow/s8.JPG',
+    caption: (<small>
+                The MIT IEEE/ACM Club represents the student branch for two
+                professional international organizations, IEEE and ACM. <br/>
+                Our mission is to create and support a tight-knit community among
+                the students and faculty in the EECS Department at MIT.
+              </small>)
+  },
+  {
+    url: 'images/slideshow/s2.jpg',
+    caption: (<small>
+                Our chapter consists of an executive board dedicated to making sure
+                you feel welcome. We prioritize your engagement by connecting
+                freshmen to upperclassmen, undergraduates to faculty, and
+                applicants to prospective internships. <br/>
+                Get involved with all things course 6 and beyond by coming to our
+                events! You'll meet students who are just as passionate as you are
+                in the diverse color spectrum that is course 6 as well as professors
+                and leaders in the field.
+              </small>)
+  },
+  {
+    url: 'images/slideshow/s10.jpg',
+    caption: (<small>
+                We host events all throughout the year and our highlights include:
+                <br/>
+                Networking and Dinner with Companies <br/>
+                Fireside Chats with Professors <br/>
+                Discover Course 6 <br/>
+                Study Breaks and Game Nights <br/>
+                Pset Parties <br/>
+              </small>)
+  }
+]
+
 
 const HomePage: React.FC = () => {
-
-
-  const sliderStyle = {
-    width: "500px",
-    height: "300px",
-  }
-
   return (
-    <div className="homepage-container">
-        
-        <div className='py-2'>
-          MIT IEEE/ACM is the student branch for the Institute of Electrical and Electronics Engineers (IEEE) and the Association for Computing Machinery (ACM). We hope to cultivate a tight-knit community for EECS students at MIT.
-        </div>
-        <div className='button button-primary py-2 my-2'>
-          <a
-            target="_blank"
-            href="newsletter/Newsletter_ November 2022.pdf"
-            className="text-icon"
-            ><b>Our Most Recent Newsletter</b></a>
-        </div>
-        <div className='button button-primary py-2'>
-          <a target="_blank" href="https://groups.mit.edu/webmoira/list/ieee-acm-interest" 
-          className="text-icon"><b>Join Our Mailing list</b></a>
-        </div> 
+    <>
+      <EventShowcase slideImages={[ ...introSlides, ...eventSlides.slice(0, 5) ]} />
+      <div className="container">
 
-      {/* <div style={sliderStyle}> 
-        <Slideshow />
-      </div> */}
-    </div>
+        <div className="my-5">
+          <div className="py-2">
+            MIT IEEE/ACM is the student branch for the Institute of Electrical and Electronics Engineers (IEEE) and the Association for Computing Machinery (ACM). We hope to cultivate a tight-knit community for EECS students at MIT.
+          </div>
+          <button className='button button-primary py-2 my-2'>
+            <a
+              target="_blank"
+              href="newsletter/Newsletter_ November 2022.pdf"
+              className="text-icon w-100"
+              ><b>Our Most Recent Newsletter</b></a>
+          </button>
+          <button className='button button-primary py-2'>
+            <a target="_blank" href="https://groups.mit.edu/webmoira/list/ieee-acm-interest" 
+            className="text-icon"><b>Join Our Mailing list</b></a>
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
