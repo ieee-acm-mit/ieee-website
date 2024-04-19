ffa6459ec9a91d364b6703410cc5639e6bd3f2ea
Jophy <jophyyjh@gmail.com>
2024-04-19 15:42:26 -0400

    Re-implemented (Responsive) NavBar, not done yet

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,52 +1,47 @@
 import { NavLink } from 'react-router-dom';
-import '../css/NavBar.css';
+import { useState } from 'react';
+import '../css/navbar.css';
 import Logo from '../../images/logo_w_background.png';
 
-export function NavBar() {
+
+function NavBar() {
+  const [expand, setExpand] = useState(false);
+
   return (
-
-    <div className="navbar">
-        <div className="navbar-left">
-          <img src={Logo} className="navbar-logo" />
-          <div>
-            MIT IEEE/ACM
-          </div>
-        </div>
-      
-        
-        <div className='navbar-right'>
-            <div>
-              <NavLink to="/">Home</NavLink>
-            </div>
-            <div>
-              <NavLink to="/about">About</NavLink>
-            </div>
-            <div>
-              <NavLink to="/team">Team</NavLink>
-            </div>
-        
-            <div>
-              <a
-                href="https://www.facebook.com/Ieeeacmmit/"
-                className="fa-brands fa-facebook"
-              ></a>
-            </div>
-            <div>
-              <a
-                href="https://www.instagram.com/mit_ieee_acm/"
-                className="fa-brands fa-instagram"
-              ></a>
-            </div>
-            <div>
-              <a
-                href="https://medium.com/mit-ieee"
-                className="fa-brands fa-medium"
-              ></a>
-            </div>
-        </div>
-    </div>
-
     
+    <nav className="navbar navbar-expand-md sticky-top">
+      <div className="navbar-left">
+        <img src={Logo} className="navbar-logo" />
+        <div>
+          MIT IEEE/ACM
+        </div>
+      </div>
+      <div className="toggle-menu d-md-none"
+            aria-controls="navbarSupportedContent" aria-expanded={expand}
+            aria-label="Toggle navigation" onClick={() => setExpand(!expand)}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+
+      <div className={`navbar-right navbar-collapse collapse ${expand ? "show" : ""}`}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/event">Events</NavLink>
+        <NavLink to="/team">Team</NavLink>
+        <a
+          href="https://www.facebook.com/Ieeeacmmit/"
+          className="fa-brands fa-facebook logo"
+        ></a>
+        <a
+          href="https://www.instagram.com/mit_ieee_acm/"
+          className="fa-brands fa-instagram"
+        ></a>
+        <a
+          href="https://medium.com/mit-ieee"
+          className="fa-brands fa-medium"
+        ></a>
+      </div>
+    </nav>
+
   );
 };
 
